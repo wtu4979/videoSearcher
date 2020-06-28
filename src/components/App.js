a0ac85437f19ef317566c52a6bdca0c92bb9ab44
Wilson Tu <wilson@Wilsons-MacBook-Pro.local>
2020-06-27 21:54:49 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import VideoList from '../components/VideoList';
+import youtube from '../apis/youtube';
+import VideoDetail from '../components/VideoDetail';
+
+const KEY = 'AIzaSyAgwo4EnbdG_XY4oQTo2zjttARBzYa7ZoE';
+
+
+class App extends React.Component {
+    state = { videos: [], selectedVideo: null };
+
+    componentDidMount() {
+        this.onTermSubmit('cats');
+    }
+
+    onTermSubmit = async term => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term,
+                part: "snippet",
+                maxResults: 5,
+                type: 'video',
+                key: KEY
+             }
+        });
+
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
+
+    }
+
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video});
+    }
+
+    render(){
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo}/>
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}   
+
+export default App;
