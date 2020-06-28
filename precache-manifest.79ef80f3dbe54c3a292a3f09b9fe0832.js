006a9dcf53596bb44e537bcc1f0e5922b3ccd790
Wilson Tu <wilson@Wilsons-MacBook-Pro.local>
2020-06-27 21:55:33 -0700

    Updates

---
+++ precache-manifest.79ef80f3dbe54c3a292a3f09b9fe0832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc5e3bbca390146104d570235272385",
+    "url": "/videoSearcher/index.html"
+  },
+  {
+    "revision": "c5fca2cfcef8d47b8150",
+    "url": "/videoSearcher/static/css/main.e9d99770.chunk.css"
+  },
+  {
+    "revision": "f505171a6b4079644394",
+    "url": "/videoSearcher/static/js/2.a24ee75e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/videoSearcher/static/js/2.a24ee75e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5fca2cfcef8d47b8150",
+    "url": "/videoSearcher/static/js/main.4ff296f1.chunk.js"
+  },
+  {
+    "revision": "b580a936eb1dc1a8773e",
+    "url": "/videoSearcher/static/js/runtime-main.45d63a77.js"
+  }
+]);
